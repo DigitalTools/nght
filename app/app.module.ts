c35cc24c24b0da84a86a752eb6daa193a1c688f8
Franco Cedillo <franco.cedillo@gmail.com>
2016-10-02 07:15:00 -0500

    initial Angular2 🚀

---
+++ app/app.module.ts
@@ -0,0 +1,30 @@
+import { routing } from './app.routing';
+
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent }  from './app.component';
+import { DashboardComponent }  from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    routing
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  bootstrap:    [ AppComponent ]
+  ,
+  providers: [HeroService]
+})
+
+export class AppModule { }
